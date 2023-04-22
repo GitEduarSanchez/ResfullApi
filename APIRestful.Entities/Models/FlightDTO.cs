6182949e437ade4e4179b691b9a74186e007d3f8
eduar sanchez <eduar.sanchez@newshore.es>
2023-04-22 10:47:30 -0500

    gitignore

--- APIRestful.Entities/Models/FlightDTO.cs
+++ APIRestful.Entities/Models/FlightDTO.cs
@@ -9,11 +9,9 @@ namespace APIRestful.Entities.Models
 {
     public class FlightDTO
     {
-        
         public string Origin { get; set; }
         public string Destination { get; set; }
         public double Price { get; set; }
         public Transport Transport { get; set; }
-    
     }
 }
